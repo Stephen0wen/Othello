d8df7260fb1bfee0cfcbbd8f598d39fdcdd793fd
Stephen0wen <stevejowen@gmail.com>
2024-06-24 16:16:34 +0100

    Indentifies game end condition

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -6,10 +6,18 @@ export const GameContext = createContext();
 export const GameProvider = ({ children }) => {
     const [gameState, setGameState] = useState(initialGameState);
     const [isWhiteTurn, setIsWhiteTurn] = useState(false);
+    const [isGameOver, setIsGameOver] = useState(false);
 
     return (
         <GameContext.Provider
-            value={{ gameState, setGameState, isWhiteTurn, setIsWhiteTurn }}
+            value={{
+                gameState,
+                setGameState,
+                isWhiteTurn,
+                setIsWhiteTurn,
+                isGameOver,
+                setIsGameOver,
+            }}
         >
             {children}
         </GameContext.Provider>
