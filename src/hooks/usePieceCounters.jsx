e0a0665d4ae6f2d3de97030c2dd8d3e72aef4eaf
Stephen0wen <stevejowen@gmail.com>
2024-06-24 11:56:09 +0100

    Adds functionality to score board

---
+++ src/hooks/usePieceCounters.jsx
@@ -0,0 +1,33 @@
+import { useContext, useEffect, useState } from "react";
+import { GameContext } from "../contexts/GameContext";
+
+export default function usePieceCounters() {
+    const [whiteCount, setWhiteCount] = useState(2);
+    const [blackCount, setBlackCount] = useState(2);
+
+    const { gameState } = useContext(GameContext);
+
+    useEffect(() => {
+        let newWhiteCount = 0;
+        let newBlackCount = 0;
+
+        gameState.forEach((column) => {
+            column.forEach((piece) => {
+                if (!piece) {
+                    return;
+                }
+                if (piece.value) {
+                    newWhiteCount++;
+                }
+                if (!piece.value) {
+                    newBlackCount++;
+                }
+            });
+        });
+
+        setWhiteCount(newWhiteCount);
+        setBlackCount(newBlackCount);
+    }, [gameState]);
+
+    return { whiteCount, blackCount };
+}
