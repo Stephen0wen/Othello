80dac1de24cb36a141671833adcd5ddd76e387fa
Stephen0wen <stevejowen@gmail.com>
2024-06-28 00:37:21 +0100

    Capitalises text on game end banner

--- src/components/GameEndBanner/GameEndBanner.jsx
+++ src/components/GameEndBanner/GameEndBanner.jsx
@@ -5,10 +5,10 @@ import { GameContext } from "../../contexts/GameContext";
 export default function GameEndBanner() {
     const { isGameOver } = useContext(GameContext);
 
-    if (isGameOver) {
+    if (!isGameOver) {
         return (
             <div id="banner-container">
-                <h2 id="game-end-banner">Click to start a new game...</h2>
+                <h2 id="game-end-banner">CLICK TO START A NEW GAME...</h2>
             </div>
         );
     }
