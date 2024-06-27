f8dd895470a26f6e5ef97b3e5b98c3db274ab8d8
Stephen0wen <stevejowen@gmail.com>
2024-06-28 00:35:23 +0100

    Completes end game functionality

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -6,13 +6,15 @@ import { GameContext } from "../../contexts/GameContext";
 import GameBoardSquare from "../GameBoardSquare/GameBoardSquare";
 import useGreedyAiTurn from "../../hooks/useGreedyAiTurn";
 import GameEndBanner from "../GameEndBanner/GameEndBanner";
+import useNewGame from "../../hooks/useNewGame";
 
 export default function GameBoard() {
     const { gameState } = useContext(GameContext);
+    const newGame = useNewGame();
     useGreedyAiTurn();
 
     return (
-        <div id="game-board">
+        <div id="game-board" onClick={newGame}>
             {gameState.map((column, xCoordinate) => {
                 return (
                     <div key={xCoordinate} className="game-board-column">
--- src/components/GameEndBanner/GameEndBanner.jsx
+++ src/components/GameEndBanner/GameEndBanner.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import "./GameEndBanner.css";
+import { GameContext } from "../../contexts/GameContext";
 
 export default function GameEndBanner() {
-    return (
-        <div id="banner-container">
-            <h2 id="game-end-banner">Click to start a new game...</h2>
-        </div>
-    );
+    const { isGameOver } = useContext(GameContext);
+
+    if (isGameOver) {
+        return (
+            <div id="banner-container">
+                <h2 id="game-end-banner">Click to start a new game...</h2>
+            </div>
+        );
+    }
 }
